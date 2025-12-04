56edc23667ceebd2947b5535f33e149af14f108b
1fab1ana <fabiana222777@gmail.com>
2025-12-04 19:17:29 +0100

    Implemento clase Principal y estructura del menú

---
+++ src/pricipal/Principal.java
@@ -0,0 +1,25 @@
+package pricipal;
+import sistema.gestorParking;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main (String[] args){
+
+        // Inicializamos el scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Inicializamos el gestor
+        gestorParking gestor = new gestorParking(); // Tipo de objeto, nombre de la variable. ejecuta el constructor
+
+        // Variable de control para salir del bucle
+        boolean salir = false;
+
+        while (!salir){
+            System.out.println("1. Aparcar coche\n2. Retirar coche\n3. Mostrar estado\n4. Salir del menú");
+            int opcion = sc.nextInt();
+        }
+
+
+
+    }
+}
