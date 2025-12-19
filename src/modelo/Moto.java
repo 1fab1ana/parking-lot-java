fafab3f07fc982d15573ea5e488181e7e64aea97
1fab1ana <fabiana222777@gmail.com>
2025-12-19 12:06:09 +0100

    Implementa switch con menú de opciones

---
+++ src/modelo/Moto.java
@@ -0,0 +1,7 @@
+package modelo;
+
+public class Moto extends Vehiculo{
+    public Moto (String matricula){ // Recibe el parámetro matrícula
+        super(matricula); // La matricula se transfiere a Moto
+    }
+}
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -18,8 +18,80 @@ public class Principal {
         boolean salir = false;
 
         while (!salir){
-            System.out.println("1. Aparcar coche\n2. Retirar coche\n3. Mostrar estado\n4. Salir del menú");
+            System.out.println("1. Aparcar vehículo\n2. Retirar vehículo\n3. Mostrar estado\n4. Salir del menú");
             int opcion = sc.nextInt();
+
+            switch (opcion){
+                case 1:
+
+                    System.out.println("Introduce el tipo de vehículo:");
+                    System.out.println("1. Coche\n2. Moto");
+                    int tipoVehiculo = sc.nextInt(); // Lectura del tipo de vehículo
+                    sc.nextLine(); // Limpiamos el buffer
+
+
+                    System.out.println("Introduce la matrícula. Ejemplo: 1111 AAA");
+                    String numeroMatricula = sc.nextLine();
+
+                    // Guardamos el objeto
+
+                    Vehiculo nuevoVehiculo = null; // Variable vacía
+
+                    // Si es un coche..
+                    if(tipoVehiculo == 1){
+
+                        nuevoVehiculo = new Coche(numeroMatricula); // Guardamos el nuevo coche en la variable vacía
+                        // Si es una moto..
+                    } else if (tipoVehiculo == 2) {
+                        nuevoVehiculo = new Moto(numeroMatricula); // Guardamos la nueva moto en la variable vacía
+                    } else {
+                        System.out.println("Tipo de vehículo inválido");
+                    }
+
+                    // Llamamos al metodo para aparcarlo
+                    if(gestor.aparcarVehiculo(nuevoVehiculo)){
+                        System.out.println("Vehículo aparcado correctamente");
+                    } else {
+                        System.out.println("No se pudo aparcar el vehículo, parking lleno");
+                    }
+
+                    break;
+
+                case 2:
+
+                    // Limpiamos el buffer
+                    sc.nextLine();
+
+                    // Pedimos el identificador (matricula) al usuario
+                    System.out.println("Introduce la matrícula. Ejemplo: 1111 AAA");
+                    String numeroMatriculaRetirar = sc.nextLine();
+
+                    Vehiculo vehiculoRetirado = gestor.retirarVehiculo(numeroMatriculaRetirar);
+
+                    if(vehiculoRetirado != null){
+                        System.out.println("Se ha retirado el vehículo correctamente");
+
+                    }else {
+                        System.out.println("No se pudo retirar el vehículo");
+                    }
+
+                    break;
+
+                case 3:
+                    // Limpiamos el buffer
+                    sc.nextLine();
+                    gestor.mostrarEstado();
+                    break;
+
+                case 4:
+                    salir = true;
+                    System.out.println("Saliendo del menú");
+                    break;
+
+                default:
+                    System.out.println("Opción inválida");
+                    break;
+            }
         }
 
 
